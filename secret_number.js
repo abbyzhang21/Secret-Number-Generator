1cf3341b72d01d57b0904f54adbffb2f2a7ee505
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-07-07 15:40:57 -1000

    secret_number complete

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,10 @@
 'use strict';
 module.exports = function() {
-  
+   const secretNumber = Math.random() * 1000001;
+   const anotherSecretNumber = Math.random() * 1000001;
+   return function(){
+       return secretNumber;
+       return anotherSecretNumber;
+   }
+    
 };
